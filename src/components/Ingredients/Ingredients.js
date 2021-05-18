1528d700b95e73a99c7cbb09d06f8d5322214767
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-05-19 02:55:58 +0300

    hooks

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import idGen from "../../helper/idGenerator";
 
 import IngredientForm from "./IngredientForm";
@@ -7,20 +7,40 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  const fetchNewList = newList => {
+  const fetchNewList = useCallback((newList = null, filterText = null) => {
+    console.log("fetching");
     fetch(
       "https://order-meal-a2f7a-default-rtdb.firebaseio.com/ingredients.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(newList),
-        headers: { "Content-Type": "application/json" },
-      }
+      newList
+        ? {
+            method: "PUT",
+            body: JSON.stringify(newList),
+            headers: { "Content-Type": "application/json" },
+          }
+        : null
     )
       .then(repsonse => repsonse.json())
       .then(data => {
-        setUserIngredients(newList);
+        if (!newList) newList = data ? data : [];
+        setUserIngredients(
+          !filterText
+            ? newList
+            : newList.filter(ingredient =>
+                ingredient.title.includes(filterText)
+              )
+        );
       });
-  };
+  }, []);
+  //after and every render cycle useEffect runs
+  // empty dependency array []=componentDidMount
+  useEffect(() => {
+    fetchNewList();
+  }, []);
+
+  useEffect(() => {
+    console.log("rendering ingredients", userIngredients);
+  }, [userIngredients]);
+
   const addIngredientHandler = ingredient => {
     if (!ingredient.amount) return;
     const index = userIngredients.findIndex(
@@ -40,12 +60,20 @@ const Ingredients = () => {
     const newList = userIngredients.filter(ingredient => ingredient.id !== id);
     fetchNewList(newList);
   };
+
+  const filterIngredientHandler = useCallback(
+    filterText => {
+      fetchNewList(null, filterText);
+    },
+    [fetchNewList]
+  );
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onSearch={filterIngredientHandler} />
         <IngredientList
           ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler}
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
 const Search = React.memo(props => {
+  const { onSearch } = props;
+  const [filter, setFilter] = useState("");
+  useEffect(() => {
+    onSearch(filter);
+  }, [filter, onSearch]);
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={filter}
+            onChange={e => setFilter(e.target.value.trim())}
+          />
         </div>
       </Card>
     </section>
